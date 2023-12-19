f081f066c7dd08b693069afa71a49196f2559140
Anumm Irshad <anummirshad@gmail.com>
2023-12-19 14:53:27 +0530

    first commit

---
+++ controllers/UserController.js
@@ -0,0 +1,30 @@
+/*Make a controller for user management */ 
+Path: controllers/UserController.js
+ import { PrismaClient } from "@prisma/client";
+
+ export const getAllUsers = async (req, res, next) => {
+  try {
+    const prisma = new PrismaClient();
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        fullName: true,
+        description: true,
+        profileImage: true,
+        userRole: true,
+        isProfileInfoSet: true,
+        createdAt: true,
+        facebookLink: true,
+        instagramLink: true,
+        githubLink: true,
+        youtubeLink: true,
+      },
+    });
+    return res.status(200).json({ users });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal Server Error");
+  }
+};
